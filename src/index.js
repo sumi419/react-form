aef73815d3c4f2a40a26b8ac07cd0932bfcc2d5e
sumi419 <sasgar04@gmail.com>
2019-09-13 15:01:06 -0700

    ✨Clean up imports in index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './static/lib/serviceWorker';
+
+import './static/css/index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
